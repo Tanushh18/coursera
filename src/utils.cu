1d1ade6de8d0337e4ae6a05bd3d83d216b6cf787
Tanushh18 <153216335+Tanushh18@users.noreply.github.com>
2025-11-15 17:14:36 +0530

    Update utils.cu

--- src/utils.cu
+++ src/utils.cu
@@ -1,1 +1,15 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+unsigned char* loadImage(const char* path, int* w, int* h) {
+    int channels;
+    unsigned char* data = stbi_load(path, w, h, &channels, 1); 
+    return data;
+}
+
+void saveImage(const char* path, unsigned char* data, int w, int h) {
+    stbi_write_jpg(path, w, h, 1, data, 90);
+}
