0c6b9206a0328fe8a10a97a7b7e98ef7d69420e2
Tanushh18 <153216335+Tanushh18@users.noreply.github.com>
2025-11-15 17:14:09 +0530

    Update kernels.cuh

--- src/kernels.cuh
+++ src/kernels.cuh
@@ -1,1 +1,9 @@
+#ifndef KERNELS_CUH
+#define KERNELS_CUH
 
+#include <cuda_runtime.h>
+
+void gaussianBlurGPU(unsigned char* input, unsigned char* output, int width, int height);
+void sobelEdgeGPU(unsigned char* input, unsigned char* output, int width, int height);
+
+#endif
